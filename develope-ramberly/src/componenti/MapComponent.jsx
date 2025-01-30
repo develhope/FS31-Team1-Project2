a632fa48067460d7c52ce009b0a6fdbd1b743e23
Marco Grassi <marco.grassi.private@gmail.com>
2025-01-30 15:09:25 +0100

    add "screenshot"  feat

--- develope-ramberly/src/componenti/MapComponent.jsx
+++ develope-ramberly/src/componenti/MapComponent.jsx
@@ -4,55 +4,56 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { useNavigate } from "react-router-dom";
 
 export function MapComponent(width) {
-    //impostiamo i riferimenti per la manipolzione della mappa e per la gestione del suo conteniutore
+  //impostiamo i riferimenti per la manipolzione della mappa e per la gestione del suo conteniutore
   const mapRef = useRef();
   const mapContainerRef = useRef();
 
   //imposto lo useState della posizione del mio utente per gestirla in seguito all'interno dello useEffect e determinare quindi le coordinate dello user
   const [userLocation, setUserLocation] = useState(null);
-  const [marker, setMarker] = useState(null);  // Per tenere traccia del marker aggiunto
-  const [distance, setDistance] = useState(null);  // Stato per la distanza calcolata
+  const [marker, setMarker] = useState(null); // Per tenere traccia del marker aggiunto
+  const [distance, setDistance] = useState(null); // Stato per la distanza calcolata
   const [routeLayer, setRouteLayer] = useState(null); // Stato per il layer del percorso
-  const [searchQuery, setSearchQuery] = useState(''); // Stato per la query di ricerca
+  const [searchQuery, setSearchQuery] = useState(""); // Stato per la query di ricerca
   const [suggestions, setSuggestions] = useState([]); // Stato per memorizzare i suggerimenti
   const navTo = useNavigate();
 
   useEffect(() => {
-        //controllo l'esistenza di navigator.geolocation prima di andare a recuperare le informazioni delle coordinate
+    //controllo l'esistenza di navigator.geolocation prima di andare a recuperare le informazioni delle coordinate
     if (navigator.geolocation) {
-            //uso getcurrent position per ottenere i valori di latitudine e longitudine e li destrutturo partendo dal position.coords
+      //uso getcurrent position per ottenere i valori di latitudine e longitudine e li destrutturo partendo dal position.coords
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
           setUserLocation([longitude, latitude]);
         },
-                //in caso di errore nel caricamento della posizione imposto una posizione generica di render, in questo caso newyork
+        //in caso di errore nel caricamento della posizione imposto una posizione generica di render, in questo caso newyork
         (error) => {
           console.error("Error fetching location:", error.message);
           setUserLocation([-74.006, 40.7128]);
         }
       );
     } else {
-            //se navigator.geolocation non è a tru imposto un else per avere in console un errore e impostare comunque la mia posizione di default
+      //se navigator.geolocation non è a tru imposto un else per avere in console un errore e impostare comunque la mia posizione di default
       console.error("Geolocation is not supported by your browser.");
       setUserLocation([-74.006, 40.7128]);
     }
   }, []);
 
   useEffect(() => {
-     //controllo che userLocation esista prima di operare ulteriormente
+    //controllo che userLocation esista prima di operare ulteriormente
     if (!userLocation) return;
- //impostiamo un valore a mapboxgl.accessToken per avere il riferimento al profilo utilizzato per la libreria
+    //impostiamo un valore a mapboxgl.accessToken per avere il riferimento al profilo utilizzato per la libreria
     mapboxgl.accessToken =
       "pk.eyJ1Ijoia2FyYXN1MDBnIiwiYSI6ImNtNmMxb2RjODBjNGQyanNjYWh2anl3aDYifQ.6Y73zxmEAj307vBJe-AmIw";
- //richiamno i riferimenti alla mappa e al suo contenitore per inizializzare la mappa
+    //richiamno i riferimenti alla mappa e al suo contenitore per inizializzare la mappa
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: userLocation,
       zoom: 10,
+      preserveDrawingBuffer: true,
     });
-   // Aggiungo il marker alla mappa
+    // Aggiungo il marker alla mappa
     const userMarker = new mapboxgl.Marker()
       .setLngLat(userLocation)
       .addTo(mapRef.current);
@@ -92,7 +93,7 @@ export function MapComponent(width) {
 
   // Funzione per calcolare la rotta
   const calculateRoute = async (destination) => {
-    if (!userLocation || !destination) return;// mi assicuro che ci siano sia la posizione dell'utente che il marker
+    if (!userLocation || !destination) return; // mi assicuro che ci siano sia la posizione dell'utente che il marker
 
     const url = `https://api.mapbox.com/directions/v5/mapbox/walking/${userLocation[0]},${userLocation[1]};${destination[0]},${destination[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`;
 
@@ -115,28 +116,28 @@ export function MapComponent(width) {
 
         const newRouteLayer = {
           id: "route", // Identificativo unico per il layer della linea
-          type: "line",// Tipo di layer: in questo caso una linea per rappresentare il percorso
+          type: "line", // Tipo di layer: in questo caso una linea per rappresentare il percorso
           source: {
-            type: "geojson",// Tipo di sorgente: formato GeoJSON per rappresentare i dati geografici
+            type: "geojson", // Tipo di sorgente: formato GeoJSON per rappresentare i dati geografici
             data: {
               type: "Feature", // Specifica che stiamo usando una "feature" GeoJSON
-              properties: {},// Proprietà opzionali della feature (può essere usato per metadati)
+              properties: {}, // Proprietà opzionali della feature (può essere usato per metadati)
               geometry: {
-                type: "LineString",// Tipo di geometria: una linea con coordinate connesse
-                coordinates: route,// Coordinate del percorso ottenute dalla Directions API
+                type: "LineString", // Tipo di geometria: una linea con coordinate connesse
+                coordinates: route, // Coordinate del percorso ottenute dalla Directions API
               },
             },
           },
           layout: {
-            "line-join": "round",// Unisce i segmenti della linea con angoli arrotondati
+            "line-join": "round", // Unisce i segmenti della linea con angoli arrotondati
             "line-cap": "round", // Termina le estremità della linea in modo arrotondato
           },
           paint: {
-            "line-color": "#ff0000",// Colore della linea: rosso (#ff0000)
-            "line-width": 4,// Spessore della linea in pixel
+            "line-color": "#ff0000", // Colore della linea: rosso (#ff0000)
+            "line-width": 4, // Spessore della linea in pixel
           },
         };
-// Aggiungi la rotta alla mappa
+        // Aggiungi la rotta alla mappa
         mapRef.current.addLayer(newRouteLayer);
         setRouteLayer(newRouteLayer);
       }
@@ -172,7 +173,7 @@ export function MapComponent(width) {
 
       if (firstResult) {
         const [longitude, latitude] = firstResult.center;
-    // Centriamo la mappa sulla posizione trovata
+        // Centriamo la mappa sulla posizione trovata
         mapRef.current.flyTo({
           center: [longitude, latitude],
           zoom: 15,
@@ -224,6 +225,16 @@ export function MapComponent(width) {
     calculateRoute([longitude, latitude]);
   };
 
+  const takeScreenshot = () => {
+    if (mapRef.current) {
+      const canvas = mapRef.current.getCanvas();
+      canvas.toBlob((blob) => {
+        const url = URL.createObjectURL(blob);
+        localStorage.setItem("mapScreenshotURL", url);
+      }, "image/png");
+    }
+  };
+
   return (
     <>
       <div>
@@ -251,14 +262,19 @@ export function MapComponent(width) {
               handleSearch();
             }}
           />
-          <button className="btn-search" onClick={handleSearch}>Cerca</button>
+          <button className="btn-search" onClick={handleSearch}>
+            Cerca
+          </button>
         </div>
 
         {/* Mostriamo i suggerimenti sotto il campo di ricerca */}
         {suggestions.length > 0 && (
           <ul className="suggestions-list">
             {suggestions.map((suggestion, index) => (
-              <li key={index} onClick={() => handleSuggestionSelect(suggestion)}>
+              <li
+                key={index}
+                onClick={() => handleSuggestionSelect(suggestion)}
+              >
                 {suggestion.place_name}
               </li>
             ))}
@@ -285,6 +301,7 @@ export function MapComponent(width) {
       >
         Add Marker
       </button>
+      <button onClick={takeScreenshot}>create screenshot</button>
 
       {distance && (
         <div>
